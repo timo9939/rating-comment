616c28a810865ac62264fc8403abb6481a30670a
timo9939 <timothylau20199@gmail.com>
2023-10-08 14:21:06 +0100

    rating function return success

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-let submitedBtn=document.getElementById("submit");
-let score=document.getElementById("rating").value
+let submitedBtn = document.getElementById("submit");
+let score = document.getElementById("ratingSpan")
+
+const btn1 = document.getElementById("1")
+const btn2 = document.getElementById("2")
+const btn3 = document.getElementById("3")
+const btn4 = document.getElementById("4")
+const btn5 = document.getElementById("5")
+const buttons = document.querySelectorAll('.buttonforRate')
 
 
-submitedBtn.addEventListener("click",submitRate )
+submitedBtn.addEventListener("click", submitRate)
 
-function submitRate(){
+let rateSelect
+buttons.forEach((rateBtn) =>{
+    rateBtn.addEventListener("click",()=>{
+       rateSelect=rateBtn.textContent
+       return rateSelect
+    })
+} 
+)
 
-    document.getElementById("mainPage").style.display="none";
-console.log("Button Clicked");
-document.getElementById("submitedPage").style.display="block"
+function submitRate() {
+
+    document.getElementById("mainPage").style.display = "none";
+    console.log("Button Clicked");
+    document.getElementById("submitedPage").style.display = "block"
+
+
+    console.log("rateSelect is ", rateSelect)
+    console.log("rateSelect Data Type is ", typeof (rateSelect))
+    score.innerHTML = rateSelect
+
 }
 
 
-score
+
+
+
 
 console.log("js work")
